c8dd71acb72039731d4f7b75480bf4f45d1e233b
Kerrick Long <me@kerricklong.com>
2017-07-24 15:29:51 -0500

    Add more tests and make them pass

--- tests/lib/rules/use-brace-expansion.js
+++ tests/lib/rules/use-brace-expansion.js
@@ -16,12 +16,20 @@ eslintTester.run('use-brace-expansion', rule, {
     { code: '{ test: computed(function() {}) }' },
     { code: '{ test: computed("a.test", "b.test", function() {}) }' },
     { code: '{ test: computed("a.{test,test2}", "b", function() {}) }' },
+    { code: '{ test: computed("a.{test,test2}", "c", "b", function() {}) }' },
     { code: '{ test: computed("model.a.{test,test2}", "model.b.{test3,test4}", function() {}) }' },
+    { code: '{ test: computed("foo.bar.{name,place}", "foo.qux.[]", "foo.baz.{thing,@each.stuff}", function() {}) }' },
     { code: "{ test: Ember.computed.filterBy('a', 'b', false) }" },
   ],
   invalid: [
     {
       code: '{ test: computed("a.test", "a.test2", function() {}) }',
+      errors: [{
+        message: 'Use brace expansion',
+      }],
+    },
+    {
+      code: '{ test: computed("a.{test,test2}", "c", "a.test3", function() {}) }',
       errors: [{
         message: 'Use brace expansion',
       }],
